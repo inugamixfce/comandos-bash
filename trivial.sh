c3ee1b3625056faf6f27cbf976a378608cc2f5e8
inugamixfce <inugamixfce@gmail.com>
2018-02-01 15:06:16 -0400

    Nuevo Archivo

---
+++ trivial.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+clear
+echo "Bienvenidos a GnuTrivial."
+sleep 2
+echo "Soy el típico juego de preguntas y respuestas."
+sleep 2
+echo "Si aciertas todos las preguntas, te concedere el titulo de magister del universo."
+echo "¿Como se llamaba el ultimo emperador Romano de occidente, claudio, teodosio
+o romulo."
+read respuesta1
+if test $respuesta1 = romulo
+then
+echo "Respuesta correcta."
+else
+echo "Lo siento, la respuesta correcta es: romulo."
+fi
+sleep 2
+echo "Pasemos a la siguiente pregunta. ¿Qué célebre filosofo Griego tuvó por discipulo
+a Alejandro Magno, plat{on, aristoteles o zenon?"
+read respuesta2
+if test $respuesta2 = aristoteles
+echo "respusta correcta."
+else
+echo "Lo siento, la respuesta correcta es: Aristoteles."
+fi
+if test $respuesta1 = romulo
+   test $respuesta2 = aristoteles
+then 
+echo "Eres un pequeño magister del universo."
+fi
